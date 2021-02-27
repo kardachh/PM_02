e3f016b958df9a8a3cf17c196b17027cc93f1294
kardachh <kardakov.denis49@gmail.com>
2021-02-27 12:52:59 +0300

    Добавьте файлы проекта.

---
+++ marathon_skills_2021/forms/FormMarathon.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace marathon_skills_2021
+{
+    partial class FormMarathon
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMarathon));
+            this.pictureBoxBackGround = new System.Windows.Forms.PictureBox();
+            this.labelMarathon = new System.Windows.Forms.Label();
+            this.labelPlaceDate = new System.Windows.Forms.Label();
+            this.pictureBoxTimer = new System.Windows.Forms.PictureBox();
+            this.labelBefore = new System.Windows.Forms.Label();
+            this.labelTimer = new System.Windows.Forms.Label();
+            this.buttonRunner = new System.Windows.Forms.Button();
+            this.buttonSponsor = new System.Windows.Forms.Button();
+            this.buttonAbout = new System.Windows.Forms.Button();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBackGround)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTimer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxBackGround
+            // 
+            this.pictureBoxBackGround.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pictureBoxBackGround.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxBackGround.Name = "pictureBoxBackGround";
+            this.pictureBoxBackGround.Size = new System.Drawing.Size(790, 125);
+            this.pictureBoxBackGround.TabIndex = 0;
+            this.pictureBoxBackGround.TabStop = false;
+            // 
+            // labelMarathon
+            // 
+            this.labelMarathon.AutoSize = true;
+            this.labelMarathon.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.labelMarathon.Font = new System.Drawing.Font("Rockwell", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMarathon.ForeColor = System.Drawing.Color.White;
+            this.labelMarathon.Location = new System.Drawing.Point(405, 9);
+            this.labelMarathon.Name = "labelMarathon";
+            this.labelMarathon.Size = new System.Drawing.Size(357, 35);
+            this.labelMarathon.TabIndex = 1;
+            this.labelMarathon.Text = "MARATHON SKILLS 2021";
+            // 
+            // labelPlaceDate
+            // 
+            this.labelPlaceDate.AutoSize = true;
+            this.labelPlaceDate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.labelPlaceDate.Font = new System.Drawing.Font("Rockwell", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPlaceDate.ForeColor = System.Drawing.Color.Gray;
+            this.labelPlaceDate.Location = new System.Drawing.Point(568, 44);
+            this.labelPlaceDate.Name = "labelPlaceDate";
+            this.labelPlaceDate.Size = new System.Drawing.Size(194, 70);
+            this.labelPlaceDate.TabIndex = 2;
+            this.labelPlaceDate.Text = "Kirov, Russia\r\nMarch 8";
+            this.labelPlaceDate.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // pictureBoxTimer
+            // 
+            this.pictureBoxTimer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pictureBoxTimer.Location = new System.Drawing.Point(0, 450);
+            this.pictureBoxTimer.Name = "pictureBoxTimer";
+            this.pictureBoxTimer.Size = new System.Drawing.Size(790, 60);
+            this.pictureBoxTimer.TabIndex = 3;
+            this.pictureBoxTimer.TabStop = false;
+            // 
+            // labelBefore
+            // 
+            this.labelBefore.AutoSize = true;
+            this.labelBefore.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.labelBefore.Font = new System.Drawing.Font("Rockwell", 22F);
+            this.labelBefore.ForeColor = System.Drawing.Color.White;
+            this.labelBefore.Location = new System.Drawing.Point(10, 457);
+            this.labelBefore.Name = "labelBefore";
+            this.labelBefore.Size = new System.Drawing.Size(234, 35);
+            this.labelBefore.TabIndex = 4;
+            this.labelBefore.Text = "Before the start:";
+            // 
+            // labelTimer
+            // 
+            this.labelTimer.AutoSize = true;
+            this.labelTimer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.labelTimer.Font = new System.Drawing.Font("Rockwell", 22F);
+            this.labelTimer.ForeColor = System.Drawing.Color.White;
+            this.labelTimer.Location = new System.Drawing.Point(250, 457);
+            this.labelTimer.Name = "labelTimer";
+            this.labelTimer.Size = new System.Drawing.Size(97, 35);
+            this.labelTimer.TabIndex = 5;
+            this.labelTimer.Text = "Timer";
+            // 
+            // buttonRunner
+            // 
+            this.buttonRunner.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.buttonRunner.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRunner.Font = new System.Drawing.Font("Rockwell", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRunner.ForeColor = System.Drawing.Color.White;
+            this.buttonRunner.Location = new System.Drawing.Point(218, 132);
+            this.buttonRunner.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonRunner.Name = "buttonRunner";
+            this.buttonRunner.Size = new System.Drawing.Size(350, 70);
+            this.buttonRunner.TabIndex = 6;
+            this.buttonRunner.Text = "I want to be Runner";
+            this.buttonRunner.UseVisualStyleBackColor = false;
+            this.buttonRunner.Click += new System.EventHandler(this.buttonRunner_Click);
+            // 
+            // buttonSponsor
+            // 
+            this.buttonSponsor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.buttonSponsor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSponsor.Font = new System.Drawing.Font("Rockwell", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSponsor.ForeColor = System.Drawing.Color.White;
+            this.buttonSponsor.Location = new System.Drawing.Point(218, 212);
+            this.buttonSponsor.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonSponsor.Name = "buttonSponsor";
+            this.buttonSponsor.Size = new System.Drawing.Size(350, 70);
+            this.buttonSponsor.TabIndex = 7;
+            this.buttonSponsor.Text = "I want to be Sponsor";
+            this.buttonSponsor.UseVisualStyleBackColor = false;
+            this.buttonSponsor.Click += new System.EventHandler(this.buttonSponsor_Click);
+            // 
+            // buttonAbout
+            // 
+            this.buttonAbout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.buttonAbout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAbout.Font = new System.Drawing.Font("Rockwell", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAbout.ForeColor = System.Drawing.Color.White;
+            this.buttonAbout.Location = new System.Drawing.Point(218, 292);
+            this.buttonAbout.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonAbout.Name = "buttonAbout";
+            this.buttonAbout.Size = new System.Drawing.Size(350, 70);
+            this.buttonAbout.TabIndex = 8;
+            this.buttonAbout.Text = "More about the event";
+            this.buttonAbout.UseVisualStyleBackColor = false;
+            this.buttonAbout.Click += new System.EventHandler(this.buttonAbout_Click);
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.buttonLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonLogin.Font = new System.Drawing.Font("Rockwell", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLogin.ForeColor = System.Drawing.Color.White;
+            this.buttonLogin.Location = new System.Drawing.Point(326, 372);
+            this.buttonLogin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(131, 70);
+            this.buttonLogin.TabIndex = 9;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = false;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // FormMarathon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(774, 501);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.buttonAbout);
+            this.Controls.Add(this.buttonSponsor);
+            this.Controls.Add(this.buttonRunner);
+            this.Controls.Add(this.labelTimer);
+            this.Controls.Add(this.labelBefore);
+            this.Controls.Add(this.pictureBoxTimer);
+            this.Controls.Add(this.labelPlaceDate);
+            this.Controls.Add(this.labelMarathon);
+            this.Controls.Add(this.pictureBoxBackGround);
+            this.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(790, 540);
+            this.MinimumSize = new System.Drawing.Size(790, 540);
+            this.Name = "FormMarathon";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Marathon 2021";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBackGround)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTimer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxBackGround;
+        private System.Windows.Forms.Label labelMarathon;
+        private System.Windows.Forms.Label labelPlaceDate;
+        private System.Windows.Forms.PictureBox pictureBoxTimer;
+        private System.Windows.Forms.Label labelBefore;
+        private System.Windows.Forms.Label labelTimer;
+        private System.Windows.Forms.Button buttonRunner;
+        private System.Windows.Forms.Button buttonSponsor;
+        private System.Windows.Forms.Button buttonAbout;
+        private System.Windows.Forms.Button buttonLogin;
+    }
+}
+
---
+++ marathon_skills_2021/forms/FormMarathon.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+    1)сделать таймер -
+    2)дописать все кнопки -
+    3)скрытие окна при открытии нового -
+*/
+
+namespace marathon_skills_2021
+{
+    public partial class FormMarathon : Form
+    {
+        public FormMarathon()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRunner_Click(object sender, EventArgs e)
+        {
+            Form formWhatRunner = new forms.FormWhatRunner();
+            formWhatRunner.Show();
+        }
+
+        private void buttonSponsor_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonAbout_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
