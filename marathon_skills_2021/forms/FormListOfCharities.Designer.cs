cdcfe666018b42c68f80a9a56d391b7789cf8265
kardachh <kardakov.denis49@gmail.com>
2021-03-02 17:21:30 +0300

    add form listOfCharity

--- marathon_skills_2021/forms/FormListOfCharities.Designer.cs
+++ marathon_skills_2021/forms/FormListOfCharities.Designer.cs
@@ -37,8 +37,22 @@
             this.labelTimer = new System.Windows.Forms.Label();
             this.buttonBack = new System.Windows.Forms.Button();
             this.timer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBoxLogo3 = new System.Windows.Forms.PictureBox();
+            this.labelDescription3 = new System.Windows.Forms.Label();
+            this.labelName3 = new System.Windows.Forms.Label();
+            this.pictureBoxLogo2 = new System.Windows.Forms.PictureBox();
+            this.labelDescription2 = new System.Windows.Forms.Label();
+            this.labelName2 = new System.Windows.Forms.Label();
+            this.pictureBoxLogo1 = new System.Windows.Forms.PictureBox();
+            this.labelDescription1 = new System.Windows.Forms.Label();
+            this.labelName1 = new System.Windows.Forms.Label();
+            this.labelRunnerRegistration = new System.Windows.Forms.Label();
+            this.labelPlease = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBackGround)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTimer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBoxBackGround
@@ -114,11 +128,152 @@
             this.timer.Enabled = true;
             this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
+            // pictureBoxLogo3
+            // 
+            this.pictureBoxLogo3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxLogo3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxLogo3.Image")));
+            this.pictureBoxLogo3.Location = new System.Drawing.Point(53, 353);
+            this.pictureBoxLogo3.Name = "pictureBoxLogo3";
+            this.pictureBoxLogo3.Size = new System.Drawing.Size(120, 77);
+            this.pictureBoxLogo3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxLogo3.TabIndex = 117;
+            this.pictureBoxLogo3.TabStop = false;
+            // 
+            // labelDescription3
+            // 
+            this.labelDescription3.AutoSize = true;
+            this.labelDescription3.BackColor = System.Drawing.Color.Transparent;
+            this.labelDescription3.Font = new System.Drawing.Font("Rockwell", 15.75F);
+            this.labelDescription3.Location = new System.Drawing.Point(179, 376);
+            this.labelDescription3.Name = "labelDescription3";
+            this.labelDescription3.Size = new System.Drawing.Size(122, 23);
+            this.labelDescription3.TabIndex = 116;
+            this.labelDescription3.Text = "Description";
+            this.labelDescription3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // labelName3
+            // 
+            this.labelName3.AutoSize = true;
+            this.labelName3.BackColor = System.Drawing.Color.Transparent;
+            this.labelName3.Font = new System.Drawing.Font("Rockwell", 15.75F);
+            this.labelName3.Location = new System.Drawing.Point(178, 353);
+            this.labelName3.Name = "labelName3";
+            this.labelName3.Size = new System.Drawing.Size(66, 23);
+            this.labelName3.TabIndex = 115;
+            this.labelName3.Text = "Name";
+            this.labelName3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pictureBoxLogo2
+            // 
+            this.pictureBoxLogo2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxLogo2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxLogo2.Image")));
+            this.pictureBoxLogo2.Location = new System.Drawing.Point(53, 270);
+            this.pictureBoxLogo2.Name = "pictureBoxLogo2";
+            this.pictureBoxLogo2.Size = new System.Drawing.Size(120, 77);
+            this.pictureBoxLogo2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxLogo2.TabIndex = 114;
+            this.pictureBoxLogo2.TabStop = false;
+            // 
+            // labelDescription2
+            // 
+            this.labelDescription2.AutoSize = true;
+            this.labelDescription2.BackColor = System.Drawing.Color.Transparent;
+            this.labelDescription2.Font = new System.Drawing.Font("Rockwell", 15.75F);
+            this.labelDescription2.Location = new System.Drawing.Point(179, 293);
+            this.labelDescription2.Name = "labelDescription2";
+            this.labelDescription2.Size = new System.Drawing.Size(122, 23);
+            this.labelDescription2.TabIndex = 113;
+            this.labelDescription2.Text = "Description";
+            this.labelDescription2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // labelName2
+            // 
+            this.labelName2.AutoSize = true;
+            this.labelName2.BackColor = System.Drawing.Color.Transparent;
+            this.labelName2.Font = new System.Drawing.Font("Rockwell", 15.75F);
+            this.labelName2.Location = new System.Drawing.Point(179, 270);
+            this.labelName2.Name = "labelName2";
+            this.labelName2.Size = new System.Drawing.Size(66, 23);
+            this.labelName2.TabIndex = 112;
+            this.labelName2.Text = "Name";
+            this.labelName2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pictureBoxLogo1
+            // 
+            this.pictureBoxLogo1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxLogo1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxLogo1.Image")));
+            this.pictureBoxLogo1.Location = new System.Drawing.Point(53, 187);
+            this.pictureBoxLogo1.Name = "pictureBoxLogo1";
+            this.pictureBoxLogo1.Size = new System.Drawing.Size(120, 77);
+            this.pictureBoxLogo1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxLogo1.TabIndex = 111;
+            this.pictureBoxLogo1.TabStop = false;
+            // 
+            // labelDescription1
+            // 
+            this.labelDescription1.AutoSize = true;
+            this.labelDescription1.BackColor = System.Drawing.Color.Transparent;
+            this.labelDescription1.Font = new System.Drawing.Font("Rockwell", 15.75F);
+            this.labelDescription1.Location = new System.Drawing.Point(179, 210);
+            this.labelDescription1.Name = "labelDescription1";
+            this.labelDescription1.Size = new System.Drawing.Size(122, 23);
+            this.labelDescription1.TabIndex = 110;
+            this.labelDescription1.Text = "Description";
+            this.labelDescription1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // labelName1
+            // 
+            this.labelName1.AutoSize = true;
+            this.labelName1.BackColor = System.Drawing.Color.Transparent;
+            this.labelName1.Font = new System.Drawing.Font("Rockwell", 15.75F);
+            this.labelName1.Location = new System.Drawing.Point(178, 187);
+            this.labelName1.Name = "labelName1";
+            this.labelName1.Size = new System.Drawing.Size(66, 23);
+            this.labelName1.TabIndex = 109;
+            this.labelName1.Text = "Name";
+            this.labelName1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // labelRunnerRegistration
+            // 
+            this.labelRunnerRegistration.AutoSize = true;
+            this.labelRunnerRegistration.Font = new System.Drawing.Font("Rockwell", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRunnerRegistration.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.labelRunnerRegistration.Location = new System.Drawing.Point(311, 76);
+            this.labelRunnerRegistration.Name = "labelRunnerRegistration";
+            this.labelRunnerRegistration.Size = new System.Drawing.Size(185, 31);
+            this.labelRunnerRegistration.TabIndex = 118;
+            this.labelRunnerRegistration.Text = "List of Charity";
+            this.labelRunnerRegistration.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelPlease
+            // 
+            this.labelPlease.AutoSize = true;
+            this.labelPlease.Font = new System.Drawing.Font("Rockwell", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPlease.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.labelPlease.Location = new System.Drawing.Point(49, 122);
+            this.labelPlease.Name = "labelPlease";
+            this.labelPlease.Size = new System.Drawing.Size(671, 46);
+            this.labelPlease.TabIndex = 119;
+            this.labelPlease.Text = "This is a list of all charities that are supported in Marathon Skills 2021.\r\nThan" +
+    "ks for helping you support them by sponsoring runners!\r\n";
+            this.labelPlease.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // FormListOfCharities
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(774, 501);
+            this.Controls.Add(this.labelPlease);
+            this.Controls.Add(this.labelRunnerRegistration);
+            this.Controls.Add(this.pictureBoxLogo3);
+            this.Controls.Add(this.labelDescription3);
+            this.Controls.Add(this.labelName3);
+            this.Controls.Add(this.pictureBoxLogo2);
+            this.Controls.Add(this.labelDescription2);
+            this.Controls.Add(this.labelName2);
+            this.Controls.Add(this.pictureBoxLogo1);
+            this.Controls.Add(this.labelDescription1);
+            this.Controls.Add(this.labelName1);
             this.Controls.Add(this.buttonBack);
             this.Controls.Add(this.labelTimer);
             this.Controls.Add(this.labelBefore);
@@ -136,6 +291,9 @@
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormAbout_FormClosed);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBackGround)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTimer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -150,6 +308,17 @@
         private System.Windows.Forms.Label labelTimer;
         private System.Windows.Forms.Button buttonBack;
         private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox pictureBoxLogo3;
+        private System.Windows.Forms.Label labelDescription3;
+        private System.Windows.Forms.Label labelName3;
+        private System.Windows.Forms.PictureBox pictureBoxLogo2;
+        private System.Windows.Forms.Label labelDescription2;
+        private System.Windows.Forms.Label labelName2;
+        private System.Windows.Forms.PictureBox pictureBoxLogo1;
+        private System.Windows.Forms.Label labelDescription1;
+        private System.Windows.Forms.Label labelName1;
+        private System.Windows.Forms.Label labelRunnerRegistration;
+        private System.Windows.Forms.Label labelPlease;
     }
 }
 
